04fc61d7496a6414b98c05869bd213b99f584dd0
sanjeevguwal <guwalsanjeev@gmail.com>
2023-03-19 17:26:37 +0530

    Update flasksetup.py

--- flasksetup.py
+++ flasksetup.py
@@ -1,7 +1,7 @@
 from flask import Flask
 app = Flask(flasksetup)
 @app.route("https://github.com/sanjeevguwal/flasksetup/new/main")#URL leading to method
-def hello(): # Name of the method
-return("Hello World!") #indent this line
+def hello():
+  return("Hello World!") #indent this line
 if flasksetup == "__main__":
-app.run(host='0.0.0.0', port='8080') # indent this line
+  app.run(host='0.0.0.0', port='8080') # indent this line
